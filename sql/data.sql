8601cfe062ad6f597dfbf621666beeef4d1794c9
Pashchima-Kamal <pashchimakamal912@gmail.com>
2018-10-23 00:33:30 +0530

    Update data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -2,3 +2,37 @@
 
 insert into login(email, password, level) values("arvind0598@gmail.com", "pass", 0);
 insert into login(email, password, level) values("admin@admin.com", "admin1", 1);
+insert into customer(id, name, address) values(1, 'arvind', 'Apt.-32 Sugar Lane');
+insert into points(id, num) values(1, 23);
+
+insert into categories(name) values('Footwear');
+insert into items(cat_id, name, details, cost, keywords) values(1, 'Nike superun', 'Black sneakers, Rain wear',1000,'shoes, sports');
+insert into items(cat_id, name, details, cost, keywords) values(1, 'Catwalk wedges', 'Brown peep toes, Platform- 2 inches, Heel- 2 inches, Faux Leather',1500,'heels, party');
+
+insert into categories(name) values('Food');
+insert into items(cat_id, name, details, cost, keywords) values(2, 'Amul cheese', 'Full fat, white cheese',100,'food, dairy');
+insert into items(cat_id, name, details, cost, keywords) values(2, 'Britto garlic bread', 'Amaze garlic amaze bread',200,'baked, food');
+
+insert into categories(name) values('Toilet');
+insert into items(cat_id, name, details, cost, keywords) values(3, 'Lux rose', 'Bodywash, dryskin',90,'cosmetic');
+insert into items(cat_id, name, details, cost, keywords) values(3, 'Colgate', 'Maxfresh, Flavour- Mint',70,'Hygiene, toothpaste');
+
+-- stock for all 6 items
+insert into stock(id, amt) values(1, 105);
+insert into stock(id, amt) values(2, 70);
+insert into stock(id, amt) values(3, 50);
+insert into stock(id, amt) values(4, 50);
+insert into stock(id, amt) values(5, 70);
+insert into stock(id, amt) values(6, 100);
+
+insert into offers(id, amt) values(1, 100); --nike
+insert into offers(id, amt) values(2, 250); --catwalk
+
+insert into cart(id, p_id, qty) values(1, 1, 1);--nike
+insert into cart(id, p_id, qty) values(1, 3, 2);--cheese
+
+insert into orders(bill, cus_id) values(1100, 1);
+insert into order_item(order_id, product_id, qty) values(1, 1, 1);
+insert into order_item(order_id, product_id, qty) values(1, 3, 2);
+
+insert into feedback(order_id, message_id) values(1, 'Very good service');
