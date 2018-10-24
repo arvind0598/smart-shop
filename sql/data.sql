551757eb3d6b41cb185bb996e68d961aa5a443d1
arvind0598 <arvind0598@gmail.com>
2018-10-25 01:57:55 +0530

    redesigned database

--- sql/data.sql
+++ sql/data.sql
@@ -1,38 +1,23 @@
 -- insert queries go here, remember to do in order to avoid foreign key issues
 
-insert into login(email, password, level) values("arvind0598@gmail.com", "pass", 0);
+insert into login(email, password, name, address, points) values("arvind0598@gmail.com", "pass", 'Arvind S', 'Apt.-32 Sugar Lane', 23);
 insert into login(email, password, level) values("admin@admin.com", "admin1", 1);
-insert into customer(id, name, address) values(1, 'Arvind S', 'Apt.-32 Sugar Lane');
-insert into points(id, num) values(1, 23);
 
 insert into categories(name) values('Footwear');
-insert into items(cat_id, name, details, cost, keywords) values(1, 'Nike superun', 'Black sneakers, Rain wear',1000,'shoes, sports');
-insert into items(cat_id, name, details, cost, keywords) values(1, 'Catwalk wedges', 'Brown peep toes, Platform- 2 inches, Heel- 2 inches, Faux Leather',1500,'heels, party');
+insert into items(cat_id, name, details, cost, keywords, stock, offer) values(1, 'Nike superun', 'Black sneakers, Rain wear',1000,'shoes sports', 105, 100);
+insert into items(cat_id, name, details, cost, keywords, stock, offer) values(1, 'Catwalk wedges', 'Brown peep toes, Platform- 2 inches, Heel- 2 inches, Faux Leather',1500,'heels party', 70, 250);
 
 insert into categories(name) values('Food');
-insert into items(cat_id, name, details, cost, keywords) values(2, 'Amul cheese', 'Full fat, white cheese',100,'food, dairy');
-insert into items(cat_id, name, details, cost, keywords) values(2, 'Britto garlic bread', 'Amaze garlic amaze bread',200,'baked, food');
+insert into items(cat_id, name, details, cost, keywords, stock) values(2, 'Amul cheese', 'Full fat, white cheese',100,'food dairy', 50);
+insert into items(cat_id, name, details, cost, keywords, stock) values(2, 'Britto garlic bread', 'Amaze garlic amaze bread',200,'baked food', 50);
 
 insert into categories(name) values('Toilet');
-insert into items(cat_id, name, details, cost, keywords) values(3, 'Lux rose', 'Bodywash, dryskin',90,'cosmetic');
-insert into items(cat_id, name, details, cost, keywords) values(3, 'Colgate', 'Maxfresh, Flavour- Mint',70,'Hygiene, toothpaste');
+insert into items(cat_id, name, details, cost, keywords, stock) values(3, 'Lux rose', 'Bodywash, dryskin',90,'cosmetic', 70);
+insert into items(cat_id, name, details, cost, keywords, stock) values(3, 'Colgate', 'Maxfresh, Flavour- Mint',70,'hygiene toothpaste', 100);
 
--- stock for all 6 items
-insert into stock(id, amt) values(1, 105);
-insert into stock(id, amt) values(2, 70);
-insert into stock(id, amt) values(3, 50);
-insert into stock(id, amt) values(4, 50);
-insert into stock(id, amt) values(5, 70);
-insert into stock(id, amt) values(6, 100);
+insert into cart(cust_id, item_id, qty) values(1, 1, 1);
+insert into cart(cust_id, item_id, qty) values(1, 3, 2);
 
-insert into offers(id, amt) values(1, 100); 
-insert into offers(id, amt) values(2, 250); 
-
-insert into cart(c_id, p_id, qty) values(1, 1, 1);
-insert into cart(c_id, p_id, qty) values(1, 3, 2);
-
-insert into orders(bill, cus_id) values(1100, 1);
-insert into order_items(order_id, product_id, qty) values(1, 1, 1);
-insert into order_items(order_id, product_id, qty) values(1, 3, 2);
-
-insert into feedback(order_id, message_id) values(1, 'Very good service');
+insert into orders(bill, cust_id, feedback) values(1100, 1, 'Very good service'	);
+insert into order_items(order_id, item_id, qty) values(1, 1, 1);
+insert into order_items(order_id, item_id, qty) values(1, 3, 2);
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,102 +1,103 @@
 -- note: run using source command 
 -- example: source ~/Desktop/projects/smart-shop/sql/setup.sql
 
-drop database shop;
+drop database `shop`;
 
-create database shop;
+create database `shop`;
 
 use shop;
 
-create table login(
-	id int primary key auto_increment,
-	email varchar(50) unique not null,
-	password varchar(30) not null,
-	level int
+CREATE TABLE `login` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`email` varchar(50) NOT NULL UNIQUE,
+	`password` varchar(30) NOT NULL,
+	`level` int NOT NULL DEFAULT '0',
+	`name` varchar(30),
+	`address` varchar(100) DEFAULT '0',
+	`registered_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`points` int DEFAULT '0',
+	PRIMARY KEY (`id`)
 );
 
--- management
-
-create table cust_logs(
-	time_performed timestamp not null default current_timestamp,
-	email varchar(50) references login on delete cascade,
-	type varchar(20) not null
+CREATE TABLE `categories` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`name` varchar(30) NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
-create table admin_logs like cust_logs;
-
--- customer tables
-
-create table customer(
-	id int primary key references login on delete cascade,
-	name varchar(30) not null,
-	address varchar(100) default null,
-	registered_time timestamp not null default current_timestamp
+CREATE TABLE `items` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`cat_id` int NOT NULL,
+	`name` varchar(30) NOT NULL,
+	`details` varchar(200),
+	`cost` int NOT NULL,
+	`keywords` varchar(200),
+	`stock` int NOT NULL DEFAULT '1',
+	`offer` int ,
+	PRIMARY KEY (`id`)
 );
 
-create table points(
-	id int primary key references customer on delete cascade,
-	num int not null default 0
+CREATE TABLE `cart` (
+	`cust_id` int NOT NULL,
+	`item_id` int NOT NULL,
+	`qty` int NOT NULL DEFAULT '1'
 );
 
--- items
-
-create table categories(
-	id int primary key auto_increment,
-	name varchar(30) not null
+CREATE TABLE `orders` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`cust_id` int NOT NULL,
+	`bill` int NOT NULL,
+	`status` int NOT NULL DEFAULT '0',
+	`time_performed` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`feedback` varchar(200),
+	PRIMARY KEY (`id`)
 );
 
-create table items(
-	id int primary key auto_increment,
-	cat_id int references categories(id) on delete cascade,
-	name varchar(30) not null,
-	details varchar(200),
-	cost int not null,
-	keywords varchar(200)
+CREATE TABLE `order_items` (
+	`order_id` int NOT NULL,
+	`item_id` int NOT NULL,
+	`qty` int NOT NULL DEFAULT '1',
+	PRIMARY KEY (`order_id`,`item_id`)
 );
 
-create table stock(
-	id int references items on delete cascade,
-	amt int not null,
-	primary key(id, amt)
+CREATE TABLE `admin_logs` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`admin_id` int NOT NULL,
+	`action` varchar(30) NOT NULL,
+	`details` varchar(30) NOT NULL,
+	`time_performed` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`)
 );
 
-create table offers(
-	id int references item on delete cascade,
-	amt int not null,
-	primary key(id, amt)
+CREATE TABLE `cust_logs` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`cust_id` int NOT NULL,
+	`action` varchar(30) NOT NULL,
+	`details` varchar(30) NOT NULL,
+	`time_performed` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`)
 );
 
-create table cart(
-	c_id int references customer(id) on delete cascade,
-	p_id int references items(id) on delete cascade,
-	qty int not null
+CREATE TABLE `access_logs` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`email` varchar(50) NOT NULL,
+	`type` varchar(20) NOT NULL,
+	`time_performed` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`)
 );
 
--- orders
+ALTER TABLE `items` ADD CONSTRAINT `items_fk0` FOREIGN KEY (`cat_id`) REFERENCES `categories`(`id`);
 
-create table orders(
-	id int primary key auto_increment,
-	bill int not null,
-	cus_id int references customer(id) on delete cascade,
-	time_performed timestamp not null default current_timestamp
-);
+ALTER TABLE `cart` ADD CONSTRAINT `cart_fk0` FOREIGN KEY (`cust_id`) REFERENCES `login`(`id`);
 
-create table order_items(
-	order_id int references orders(id) on delete cascade,
-	product_id int references items(id) on delete cascade,
-	qty int not null,
-	primary key(order_id, product_id)
-);
+ALTER TABLE `cart` ADD CONSTRAINT `cart_fk1` FOREIGN KEY (`item_id`) REFERENCES `items`(`id`);
 
-create table del_status(
-	order_id int primary key references orders(id) on delete cascade,
-	status int not null
-);
+ALTER TABLE `orders` ADD CONSTRAINT `orders_fk0` FOREIGN KEY (`cust_id`) REFERENCES `login`(`id`);
 
-create table feedback(
-	order_id int references orders(id) on delete cascade,
-	message_id varchar(200) not null,
-	primary key(order_id, message_id)
-);
+ALTER TABLE `order_items` ADD CONSTRAINT `order_items_fk0` FOREIGN KEY (`order_id`) REFERENCES `orders`(`id`);
 
+ALTER TABLE `order_items` ADD CONSTRAINT `order_items_fk1` FOREIGN KEY (`item_id`) REFERENCES `items`(`id`);
 
+ALTER TABLE `admin_logs` ADD CONSTRAINT `admin_logs_fk0` FOREIGN KEY (`admin_id`) REFERENCES `login`(`id`);
+
+ALTER TABLE `cust_logs` ADD CONSTRAINT `cust_logs_fk0` FOREIGN KEY (`cust_id`) REFERENCES `login`(`id`);
